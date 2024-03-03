071587090d9a99458e5676f02154c9c905e54bd6
wsolano8 <wsolano@ltechec.com>
2024-03-02 19:54:16 -0500

    Cargando Ejercicios "Hello World y Calculadora"

---
+++ calculadora/main.py
@@ -0,0 +1,28 @@
+def sumar(a, b):
+    return (a+b)
+
+def restar(a, b):
+    resta = a-b
+    return resta
+
+def multiplicar(a, b):
+    return (a*b)
+
+def dividir(a, b):
+    if (b==0):
+        result = "Error: No se puede dividir por cero"
+    else:
+        result = (a/b)
+    return result
+
+def main():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+
+    print("Suma:", sumar(num1, num2))
+    print("Resta:", restar(num1, num2))
+    print("Multiplicación:", multiplicar(num1, num2))
+    print("División:", dividir(num1, num2))
+
+if __name__ == "__main__":
+    main()
